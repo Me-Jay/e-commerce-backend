f6b4cc603c763ea74449bd94d0ab76206d4138ef
Me-Jay <43656073+Me-Jay@users.noreply.github.com>
2020-10-12 15:14:37 +0000

    Update server.js

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const chalk = require('chalk');
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(chalk.greenBright(`Server start on port ${PORT}`));
+    console.log(chalk.greenBright(`Server started on port ${PORT}`));
 })
